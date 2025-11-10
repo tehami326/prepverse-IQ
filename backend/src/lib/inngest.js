7341b06d1271d12307ba1a3f2c14fb0d22e76b98
tehami326 <muhammadtehami129@gmail.com>
2025-11-10 15:48:54 +0530

    auth db sync

---
+++ backend/src/lib/inngest.js
@@ -0,0 +1,42 @@
+import { Inngest } from "inngest"
+import { connectDb } from "./db.js"
+import User from "../models/User.js"
+
+export const inngest = new Inngest({ id: "prepverse-IQ" })
+
+const syncUser = inngest.createFunction(
+    { id: "sync-user" },
+    { event: "clerk/user.created" },
+    async ({ event }) => {
+        await connectDb()
+
+        const { id, email_addressed, first_name, last_name, image_url } = event.data
+
+        const newUser = {
+            clerkId: id,
+            email: email_addressed[0]?.email_address,
+            name: `${first_name || ""} ${last_name || ""}`,
+            profileImage: image_url,
+        }
+        await User.create(newUser)
+    }
+)
+
+const deleteUserFromDB = inngest.createFunction(
+    { id: "delete_user_from_db" },
+    { event: "clerk/user.deleted" },
+    async ({ event }) => {
+        await connectDb()
+
+        const { id } = event.data
+
+
+        await User.deleteOne({ clerkId: id })
+    }
+)
+
+
+
+
+export const functions = [syncUser, deleteUserFromDB]
+
