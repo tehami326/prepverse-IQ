32132f13db8e3fbb94416a0f0314188728c547e2
tehami326 <muhammadtehami129@gmail.com>
2025-11-10 19:29:31 +0530

    msg to message updated

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -7,17 +7,17 @@ export const protectRoutes = [
         try {
             const clerkId = req.auth.userId;
 
-            if (!clerkId) return res.status(401).json({ msg: "Unauthorized - invalid token" })
+            if (!clerkId) return res.status(401).json({ message: "Unauthorized - invalid token" })
 
             const user = await User.findOne({ clerkId })
 
-            if (!user) return res.status(404).json({ msg: "User not fount" })
+            if (!user) return res.status(404).json({ message: "User not fount" })
 
             req.user = user
             next()
         } catch (error) {
             console.error("Error in protectRoute middleware", error)
-            res.status(500).json({ msg: "Internal Server error" })
+            res.status(500).json({ message: "Internal Server error" })
         }
     },
 ];
