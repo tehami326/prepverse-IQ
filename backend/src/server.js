e2b21f9c121012da77ce9ba7cb20b00808797aaf
tehami326 <muhammadtehami129@gmail.com>
2025-11-12 18:16:33 +0530

    session routes and controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,7 @@ import { serve } from "inngest/express"
 import { inngest, functions } from "./lib/inngest.js"
 import { clerkMiddleware } from '@clerk/express'
 import chatRoutes from "./routes/chatRoutes.js"
-
+import sessionRoutes from "./routes/sessionRoutes.js"
 const app = express()
 
 const __dirname = path.resolve()
@@ -18,6 +18,7 @@ app.use(clerkMiddleware())
 
 app.use("/api/inngest", serve({ client: inngest, functions }))
 app.use("/api/chat", chatRoutes)
+app.use("/api/sessions", sessionRoutes)
 
 app.get("/health", (req, res) => {
     res.status(200).json({ msg: "success from api" })
