bbf970d7aca9c5f75a2db5bb9eb536f2ed94e4e3
tehami326 <muhammadtehami129@gmail.com>
2025-11-12 18:24:49 +0530

    fixed some issues in sessionController

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -85,7 +85,15 @@ export async function joinSession(req, res) {
 
         if (!session) return res.status(404).json({ message: "Session not found" })
 
-        if (session.participant) return res.status(404).json({ message: "Session is full" });
+        if (session.status !== "active") {
+            return res.status(400).json({ message: "Cannot join the complete session" })
+        }
+
+        if (session.host.toString() === userId.toString()) {
+            return res.status(400).json({ message: "Host cannot join there own session as participant" })
+        }
+
+        if (session.participant) return res.status(409).json({ message: "Session is full" });
 
         session.participant = userId
         await session.save()
@@ -118,14 +126,17 @@ export async function endSession(req, res) {
             return res.status(404).json({ message: "Session is already completed" })
         }
 
-        session.status = "completed"
-        await session.save()
+
 
         const call = streamClient.video.call("default", session.callId)
         await call.delete({ hard: true })
 
         const channel = chatClient.channel("messaging", session.callId);
-        await channel.delete()
+        await channel.delete();
+
+        session.status = "completed";
+        await session.save();
+
         return res.status(200).json({ session, message: "Session ended successfully" })
     } catch (error) {
         console.log("Error in endSession controller", error.message)
